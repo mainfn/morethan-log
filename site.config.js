e48db6f80b0e69f02023d5569bc84edfe3617cd0
mainfn <121966058+mainfn@users.noreply.github.com>
2023-08-31 16:01:43 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -23,7 +23,7 @@ const CONFIG = {
   projects: [
     {
       name: NAME,
-      href: "https://github.com/morethanmin/morethan-log",
+      href: "https://github.com/mainfn",
     },
   ],
   // blog setting (required)
@@ -33,7 +33,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://mainfn.dev",
   since: 2023, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
