80b2830f64931743f975443a9983ee52970f5eca
mainfn <121966058+mainfn@users.noreply.github.com>
2023-08-31 16:13:44 +0900

    Update NavBar.tsx

--- src/layouts/RootLayout/Header/NavBar.tsx
+++ src/layouts/RootLayout/Header/NavBar.tsx
@@ -1,18 +1,22 @@
 import styled from "@emotion/styled"
 import Link from "next/link"
 
+const GITHUB_URL = "https://github.com/mainfn";
+
 const NavBar: React.FC = () => {
-  const links = [{ id: 1, name: "Github", to: "https://github.com/mainfn" }]
+  // const links = [{ id: 1, name: "Github", to: "https://github.com/mainfn" }]
   // const links = [];
   return (
     <StyledWrapper className="">
-      <ul>
+      <a href={GITHUB_URL}>{link.name}</a>
+{/*       <ul>
         {links.map((link) => (
           <li key={link.id}>
+            <a href={GITHUB_URL}>{link.name}</a>
             <Link href={link.to}>{link.name}</Link>
           </li>
         ))}
-      </ul>
+      </ul> */}
     </StyledWrapper>
   )
 }
