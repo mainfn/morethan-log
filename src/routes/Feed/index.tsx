6c58e5c5619682e043dffd250b7f6e9eb40b6572
mainfn <121966058+mainfn@users.noreply.github.com>
2023-08-31 16:08:43 +0900

    Update index.tsx

--- src/routes/Feed/index.tsx
+++ src/routes/Feed/index.tsx
@@ -29,7 +29,7 @@ const Feed: React.FC<Props> = () => {
         <TagList />
       </div>
       <div className="mid">
-        <MobileProfileCard />
+{/*         <MobileProfileCard /> */}
         <SearchInput value={q} onChange={(e) => setQ(e.target.value)} />
         <div className="tags">
           <TagList />
@@ -46,9 +46,9 @@ const Feed: React.FC<Props> = () => {
           height: `calc(100vh - ${HEADER_HEIGHT}px)`,
         }}
       >
-        <ProfileCard />
-        <ServiceCard />
-        <ContactCard />
+{/*         <ProfileCard /> */}
+{/*         <ServiceCard /> */}
+        // <ContactCard />
         <div className="footer">
           <Footer />
         </div>
