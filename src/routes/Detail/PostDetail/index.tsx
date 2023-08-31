6b313c00080a5ac97b11c16c7ced5b037756f4ff
mainfn <121966058+mainfn@users.noreply.github.com>
2023-08-31 16:32:38 +0900

    Update index.tsx

--- src/routes/Detail/PostDetail/index.tsx
+++ src/routes/Detail/PostDetail/index.tsx
@@ -43,6 +43,24 @@ const PostDetail: React.FC<Props> = () => {
 
 export default PostDetail
 
+// const StyledWrapper = styled.div`
+//   padding-left: 1.5rem;
+//   padding-right: 1.5rem;
+//   padding-top: 3rem;
+//   padding-bottom: 3rem;
+//   border-radius: 1.5rem;
+//   max-width: 56rem;
+//   background-color: ${({ theme }) =>
+//     theme.scheme === "light" ? "white" : theme.colors.gray4};
+//   box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+//     0 2px 4px -1px rgba(0, 0, 0, 0.06);
+//   margin: 0 auto;
+//   > article {
+//     margin: 0 auto;
+//     max-width: 42rem;
+//   }
+// `
+
 const StyledWrapper = styled.div`
   padding-left: 1.5rem;
   padding-right: 1.5rem;
@@ -50,10 +68,6 @@ const StyledWrapper = styled.div`
   padding-bottom: 3rem;
   border-radius: 1.5rem;
   max-width: 56rem;
-  background-color: ${({ theme }) =>
-    theme.scheme === "light" ? "white" : theme.colors.gray4};
-  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
-    0 2px 4px -1px rgba(0, 0, 0, 0.06);
   margin: 0 auto;
   > article {
     margin: 0 auto;
